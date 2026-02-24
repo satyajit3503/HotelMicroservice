f9ed4964691464cb9a969fe46ed1715d85e5d79a
satyajit3503 <satyajitsahu042@gmail.com>
2026-02-25 00:09:10 +0530

    added required dependancy and changes in user service to show response

--- RatingService/src/main/java/com/microService/RatingService/controller/RatingController.java
+++ RatingService/src/main/java/com/microService/RatingService/controller/RatingController.java
@@ -1,4 +1,4 @@
-package com.microService.RatingService.controller;
+	package com.microService.RatingService.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
--- Userservice/src/main/java/com/microservice/Userservice/config/userConfig.java
+++ Userservice/src/main/java/com/microservice/Userservice/config/userConfig.java
@@ -1,5 +1,6 @@
 package com.microservice.Userservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class userConfig {
 	@Bean
+    @LoadBalanced   // ⭐ THIS IS MISSING
 	public RestTemplate restTemplate() {
 
 
